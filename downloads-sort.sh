23ccf4a3d21df1559e59613be9bad320940e74be
baybled <31955015+baybled@users.noreply.github.com>
2023-05-07 11:52:33 +0100

    handles top level files in the source_dir

--- downloads-sort.sh
+++ downloads-sort.sh
@@ -26,11 +26,25 @@ done
 font_dirs=$(find "$source_dir" \( -iname '*.sf2' -o -iname '*.sfz' \) -type f -printf '%h\n' | sort -u)
 
 # Loop through each font directory and move it to the destination directory
+# Find all directories containing .sfz/.sf2 files
+font_dirs=$(find "$source_dir" \( -iname '*.sf2' -o -iname '*.sfz' \) -type f -printf '%h\n' | sort -u)
+
+# Loop through each font directory and move it to the destination directory
 for font_dir in $font_dirs; do
     # Only move the directory if it's not already in the destination directory
     if [ "$destination_dir" != "$font_dir" ]; then
-        # Move the directory to the destination directory
-        mv "$font_dir" "$destination_dir"
-        echo "Moved $font_dir to $destination_dir"
+        # Check if there are any top-level .sf2 files in the font directory
+        sf2_files=$(find "$font_dir" \( -iname '*.sf2' \) -maxdepth 1 -type f)
+        if [ -n "$sf2_files" ]; then
+            # Move the top-level .sf2 files to the destination directory
+            for sf2_file in $sf2_files; do
+                mv "$sf2_file" "$destination_dir"
+                echo "Moved $sf2_file to $destination_dir"
+            done
+        else
+            # Move the entire directory to the destination directory
+            mv "$font_dir" "$destination_dir"
+            echo "Moved $font_dir to $destination_dir"
+        fi
     fi
 done
